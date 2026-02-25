e8b6002281dbc064d533dc9221fbba952caba982
Daniel Wahlquist <triforce63@icloud.com>
2026-02-25 11:46:51 -0700

    worked an building an api, but we switched directions

--- backend/obj/Debug/net10.0/JournalApi.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/JournalApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c134dc97c060abf65454b85ad1699a1adbd28a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb8fa0ef73bbe1438182b2bba4a6645e22577a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
