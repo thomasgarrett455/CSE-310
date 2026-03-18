5786f42c74eebda1f9b0a05e7d44dc2e66e0dab0
Thomas Garrett <thomasgarrett455@gmail.com>
2026-03-18 15:47:44 -0600

    got add goals button to work, and displaying goals works now aswell

--- backend/server.js
+++ backend/server.js
@@ -172,18 +172,22 @@ app.post('/name_current_goals', async (req, res) => {
         if (!username) {
             return res.status(400).json({error: "Username not found"});
         }
+        console.log("📥 Received username:", username); // ADD THIS
+        console.log("📥 Username type:", typeof username);
 
         const [rows] = await pool.query(
-            `SELECT goals.name
+            `SELECT name
              FROM goals
              JOIN users
              ON goals.users_id = users.users_id
              WHERE users.username = ?`,
             [username],
         );
-        if (!Array.isArray(rows) || rows.length === 0 ) {
-            return res.status(401).json({ message: 'Invalid credentials'});
-        }
+        console.log("🗄️ Raw query rows:", rows); // ADD THIS
+        console.log("🗄️ Row count:", rows.length);
+        // if (!Array.isArray(rows) || rows.length === 0 ) {
+        // return res.status(401).json({ message: 'Invalid credentials'});
+        // }
         return res.status(200).json({ goals: rows });
     } catch (error) {
         console.error("Error fetching goal names", error)
@@ -201,7 +205,7 @@ app.post('/add_goal', async (req, res) => {
         }
 
         const [[user]] = await pool.query(
-            "SELECT id FROM users WHERE username = ?",
+            "SELECT users_id FROM users WHERE username = ?",
             [username]
         );
 
@@ -209,12 +213,12 @@ app.post('/add_goal', async (req, res) => {
             return res.status(404).json({ error: "User not found" });
         }
 
-        const userId = user.id;
-l
+        const userId = user.users_id;
+    
         const [result] = await pool.query(
-            `INSERT INTO goals (name, description, status, users_id)
-             VALUES (?, ?, 0, ?)`,
-            [name, content, userId]
+            `INSERT INTO goals (name, description, status, users_id, created_at)
+             VALUES ("goal123", ?, 0, ?, Now())`,
+            [content, userId] //add name back in 
         );
 
         return res.status(200).json({
